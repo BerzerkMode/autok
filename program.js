77a7139110d2ca3002b11d843b901b27f2294cab
BerzerkMode <0309679abc@gmail.com>
2026-01-30 11:29:36 +0100

    Update program.js local

--- program.js
+++ program.js
@@ -2,7 +2,7 @@ import { legjobb } from "./autok";
 
 console.log("Program elindult");
 
-for (let i=1; i<=7; i++) console.log(i, "autó");
+for (let i=1; i<=6; i++) console.log(i, "autó");
 legjobb();
 legjobb();
 
